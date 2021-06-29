5c648a980c438ed9f2afbb34b87c44a7af5202ef
decatron <chiragkrvachhhani@gmail.com>
2021-06-29 12:53:12 +0530

    2 videos added to the cctv panels

---
+++ server.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+const express = require('express');
+const app = express();
+
+
+app.get("/cctv1.html", function(req, res) {
+  res.sendFile(__dirname + "/cctv1.html");
+});
+
+app.get("/cctv2.html", function(req, res) {
+  res.sendFile(__dirname + "/examples/cctv2.html");
+});
+
+app.get("/cctv3.html", function(req, res) {
+  res.sendFile(__dirname + "/examples/cctv3.html");
+});
+
+app.get("/cctv4.html", function(req, res) {
+  res.sendFile(__dirname + "/examples/cctv4.html");
+});
+
+app.listen(3000, function() {
+  console.log("server running at port 3000");
+});
