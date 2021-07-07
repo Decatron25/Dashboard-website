f50622a2b61cc904f2f98c01e706a5bec04bd6ec
decatron <chiragkrvachhhani@gmail.com>
2021-07-07 17:45:06 +0530

    p5 shifted to backend

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 //jshint esversion:6
+
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 
 app.get("/cctv1.html", function(req, res) {
-  res.sendFile(__dirname + "/cctv1.html");
+  res.sendFile(__dirname + "public/cctv1.html");
 });
 
 app.get("/cctv2.html", function(req, res) {
-  res.sendFile(__dirname + "/examples/cctv2.html");
+  res.sendFile(__dirname + "public/cctv2.html");
 });
 
 app.get("/cctv3.html", function(req, res) {
-  res.sendFile(__dirname + "/examples/cctv3.html");
+  res.sendFile(__dirname + "public/cctv3.html");
 });
 
 app.get("/cctv4.html", function(req, res) {
-  res.sendFile(__dirname + "/examples/cctv4.html");
+  res.sendFile(__dirname + "public/cctv4.html");
 });
 
 app.listen(3000, function() {
