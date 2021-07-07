7fb72b3a4f400524272529c54fc9b45e03b04ea0
decatron <chiragkrvachhhani@gmail.com>
2021-07-07 20:00:33 +0530

    p5 shifted to backend and changes to front-end

--- server.js
+++ server.js
@@ -2,25 +2,28 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 
 app.get("/cctv1.html", function(req, res) {
   res.sendFile(__dirname + "/cctv1.html");
 });
 
 app.get("/cctv2.html", function(req, res) {
-  res.sendFile(__dirname + "/examples/cctv2.html");
+  res.sendFile(__dirname + "public/cctv2.html");
 });
 
 app.get("/cctv3.html", function(req, res) {
-  res.sendFile(__dirname + "/examples/cctv3.html");
+  res.sendFile(__dirname + "public/cctv3.html");
 });
 
 app.get("/cctv4.html", function(req, res) {
-  res.sendFile(__dirname + "/examples/cctv4.html");
+  res.sendFile(__dirname + "public/cctv4.html");
 });
+
 app.get("/facemask.html", (req,res) => {
-  res.sendFile(__dirname + "/examples/facemask.html");
-})
+  res.sendFile(__dirname + "public/facemask.html");
+});
+
 app.listen(3000, function() {
   console.log("server running at port 3000");
 });
